118d1eaa671425673a28e399be8ac1f415aa3dba
huangda <da.huang@nicezhuanye.com>
2020-04-15 23:15:22 +0800

    添加session拦截器

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,7 +1,6 @@
 package life.majiang.community.controller;
 
 import life.majiang.community.dto.QuestionDTO;
-import life.majiang.community.entity.User;
 import life.majiang.community.service.impl.QuestionServiceImpl;
 import life.majiang.community.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
@@ -23,18 +21,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(HttpServletRequest request, Model model) {
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userService.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                }
-            }
-        }
         List<QuestionDTO> list = questionService.list();
         model.addAttribute("questions", list);
         return "index";
---
+++ src/main/java/life/majiang/community/controller/ProfileController.java
@@ -0,0 +1,20 @@
+package life.majiang.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ProfileController {
+
+    @GetMapping("/profile/{action}")
+    public String profile(@PathVariable String action, Model model) {
+        if ("question".equals(action)) {
+            model.addAttribute("section", "questions");
+            model.addAttribute("sectionName", "我的提问");
+        }
+        return "profile";
+    }
+
+}
